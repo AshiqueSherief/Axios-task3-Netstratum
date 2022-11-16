27f6e5a5ed939ee49f42c2f51dfeabceea174893
Ashique Sherief <ashiquesherief.005@gmail.com>
2022-11-16 09:46:31 +0530

    task 4 netstratum

--- src/App.js
+++ src/App.js
@@ -4,30 +4,32 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [data, setData] = useState([]);
+  const [l, setL] = useState(false);
 
   useEffect(() => {
     Axios.get("https://api.publicapis.org/entries").then((a) => {
       setData(a.data.entries);
+      setL(true);
     });
   }, []);
   return (
-    <div className="App">
-      <table className="tableHeader">
+    <div className="App">{l ? <table className="tableHeader">
+      <tr className="A">
+        <th>Sl.no.</th>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Link</th>
+      </tr>
+      {data.map((e, i) => (
         <tr>
-          <th>sl.no.</th>
-          <th>Name</th>
-          <th>Description</th>
-          <th>Link</th>
+          <td>{i + 1}</td>
+          <td>{e.API}</td>
+          <td>{e.Description}</td>
+          <td>{e.Link}</td>
         </tr>
-        {data.map((e, i) => (
-          <tr>
-            <td>{i + 1}</td>
-            <td>{e.API}</td>
-            <td>{e.Description}</td>
-            <td>{e.Link}</td>
-          </tr>
-        ))}
-      </table>
+      ))}
+    </table> : <img src="loading.png" alt="Loading ..."></img>}
+
     </div>
   );
 }
